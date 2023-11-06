ade8010ad9490d18268a9548d1c39eefa6f0cc00
bestony <bestony@linux.com>
2023-11-06 23:45:57 +0800

    feat: add last modified time

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-
+import { remarkModifiedTime } from "./src/utils/remark-modified-time.mjs "
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pragmatic-guide.linux.cn',
-  integrations: []
+  integrations: [],
+  markdown:{
+    remarkPlugins:[remarkModifiedTime],
+  }
 });
