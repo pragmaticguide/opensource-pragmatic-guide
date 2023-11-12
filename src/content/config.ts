7545de3a52a56976fcb03e7cfac5df21bda18ff4
bestony <bestony@linux.com>
2023-11-12 21:50:34 +0800

    change to use remark as slide show

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,9 +3,11 @@ import { defineCollection } from 'astro:content';
 // 2. 定义集合
 const guideCollection = defineCollection({ type:'content'});
 const howToCollection = defineCollection({});
+const slidesCollection = defineCollection({type:'content'})
 // 3. 导出一个 `collections` 对象来注册集合
 //    这个键应该与 `src/content` 中的集合目录名匹配
 export const collections = {
     'guides': guideCollection,
-    'howto':howToCollection
+    'howto':howToCollection,
+    'slides': slidesCollection
 };
